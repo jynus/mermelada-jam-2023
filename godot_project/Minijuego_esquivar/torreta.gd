f81849a54a465b8ea6c420daf9ff9f8432007919
Jaime Crespo <jynus@users.noreply.github.com>
2023-11-18 10:48:40 +0100

    Modificado minijuego de disparos para que dispare :-)
    * Añadido proyectil
    * Añadido bordes
    * Lo lanza, puede impactar con jugador o con los bordes
    * Se para cuando dispara
    * Continúa moviéndose cuando el disparo impacta con el jugador o los bordes

--- godot_project/Minijuego_esquivar/torreta.gd
+++ godot_project/Minijuego_esquivar/torreta.gd
@@ -1,12 +1,29 @@
 extends Node2D
 
 @export var eye_rotation_degress: float = 2
-@onready var eye = $Sprite2D
+@onready var proyectile_spawner = $ProyectileSpawner
+var laser_scene: PackedScene = preload("res://scene_objects/proyectile.tscn")
+var is_shooting = false
 
+
+func shoot():
+	"""Stop moving and spawn a projectile"""
+	print_debug("te pillé")
+	is_shooting = true
+	var laser: Area2D = laser_scene.instantiate()
+	get_parent().add_child(laser)
+	laser.global_rotation = proyectile_spawner.global_rotation
+	laser.global_position = proyectile_spawner.global_position
+	laser.connect("impacted", shoot_finished)
+
+func shoot_finished():
+	"""Continue moving"""
+	is_shooting = false
 
 func _process(delta):
-	eye.rotation += eye_rotation_degress * delta
-	$RayCast2D.rotation += eye_rotation_degress * delta
+	if not is_shooting and $RayCast2D.is_colliding():
+		shoot()
 	
-	if $RayCast2D.is_colliding():
-		print("te pillé")
+	if not is_shooting:
+		rotation += eye_rotation_degress * delta
+
