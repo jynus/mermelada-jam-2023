98ec12106c7999ce99ea30a19ebe24bd246d7ab4
Jaime Crespo <jynus@users.noreply.github.com>
2023-11-19 21:52:46 +0100

    Deshabilitar botones durante 0.5 segundos en win y lose level para evitar pulaciones accidentales

--- godot_project/scripts/lose_level.gd
+++ godot_project/scripts/lose_level.gd
@@ -1,15 +1,21 @@
 extends Control
 
 @onready var click : AudioStreamPlayer = $Click
+@onready var repeat_level : Button = %Repeat_level
+@onready var back_main_menu : Button = %back_main_menu
+@onready var enable_buttons_timer = %EnableButtonsTimer
 
 var previous_music : String
 var previous_music_offset : float = 0
+var was_visible : bool = false
 
 func _ready():
 	visible = false
 
 func _process(delta):
-	if visible:
+	if not was_visible and visible:
+		was_visible = true
+		enable_buttons_timer.start()
 		get_tree().paused = true
 		BackgroundMusic.play_song("lose")
 
@@ -28,3 +34,9 @@ func _on_back_main_menu_pressed():
 	await click.finished
 	unpause()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_enable_buttons_timer_timeout():
+	repeat_level.disabled = false
+	back_main_menu.disabled = false
+
