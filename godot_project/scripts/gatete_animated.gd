2d708e36e69e8e27e0c32320cc548b37df8a9b1e
Jaime Crespo <jynus@users.noreply.github.com>
2023-11-19 01:17:48 +0100

    Primer batch de añadidos de montes + fix de estilo del menú
    Aún faltan cosas de arreglar del menú, pero al menos es usable.

---
+++ godot_project/scripts/gatete_animated.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+@export var ojos_movement: float = 40
+@export var ojos_lag: float = 0.5
+@onready var ojos_gatete: Sprite2D = %OjosGatete
+@onready var ojos_center = %ojosCenter
+@onready var timer = %Timer
+@onready var animation_player = %AnimationPlayer
+@onready var love_animation = %LoveAnimation
+
+var tween: Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.wait_time = ojos_lag
+
+
+func _on_timer_timeout():
+	var direction : Vector2 = (get_global_mouse_position() - ojos_center.global_position).normalized()
+	var target_position = ojos_movement * direction
+	print("target_position", target_position)
+	if tween:
+		tween.kill()
+	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(ojos_gatete, "position", target_position, ojos_lag)
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		print("Yes, you can pet the cat")
+		animation_player.speed_scale = 2
+		love_animation.emitting = true
+
+
+func _on_mouse_exited():
+	animation_player.speed_scale = 1
+	love_animation.emitting = false
