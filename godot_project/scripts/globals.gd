88871c2f03484ad66f5a0c75b8061e863f401690
Jaime Crespo <jynus@users.noreply.github.com>
2023-11-19 12:49:04 +0100

    Comentar script autoloaded "Globals"
    Así se entiende cómo se usa.

--- godot_project/scripts/globals.gd
+++ godot_project/scripts/globals.gd
@@ -1,4 +1,10 @@
 extends Node
 
-var COLOR_GOOD: Color = Color.AQUAMARINE
-var COLOR_BAD: Color = Color.CRIMSON
+# Add here global references that will be used on multiple levels/scenes
+# Use it like this: color = Globals.COLOR_GOOD
+
+var COLOR_GOOD: Color = Color.AQUAMARINE  # the color used for "good things"
+var COLOR_BAD: Color = Color.CRIMSON  # the color used for "bad things"
+
+# var FX_GOOD: String = "res://"
+# var FX_BAD: String = "res://"
