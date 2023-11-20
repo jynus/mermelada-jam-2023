25f0ba1feb2d2520dd61648f253296763e0b88ec
Meldarse <meldarse@gmail.com>
2023-11-19 20:35:05 -0500

    cambio de cursor en nivel1

--- godot_project/scripts/heart.gd
+++ godot_project/scripts/heart.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var speed : float = -200
 @onready var sprite : Sprite2D = $Sprite
 @onready var color = Globals.COLOR_GOOD
+var cat = preload("res://assets/sprites/huella_gato.png")
 
 signal missed
 signal touched
@@ -23,7 +24,10 @@ func _on_input_event(_viewport, event, _shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
 		print("Clicked")
 		hide()
+		Input.set_custom_mouse_cursor(cat)
 		$AudioStreamPlayer.play()
+		await $AudioStreamPlayer.finished
+		Input.set_custom_mouse_cursor(null)
 
 func _on_audio_stream_player_finished():
 	queue_free()
