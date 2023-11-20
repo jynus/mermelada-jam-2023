d6b4bfb21e38f59f5cde00744f7e3b8eed3fcc9e
Meldarse <meldarse@gmail.com>
2023-11-20 05:55:28 -0500

    cambio de color nave enemiga / nivel 5

--- godot_project/Minijuego3/ship_enemy.gd
+++ godot_project/Minijuego3/ship_enemy.gd
@@ -1,11 +1,17 @@
 extends RigidBody2D
 
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var color = Globals.COLOR_BAD
+
+func _ready():
+	sprite.self_modulate = color
+
 func _process(delta):
 	pass
 
+
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
-
 
 
 func _on_button_button_down():
